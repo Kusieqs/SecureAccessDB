19cf2bb26193d8d45b44b137868503559fea70c2
Kusieq <konrad.kus@edu.uekat.pl>
2024-04-03 13:57:09 +0200

    Dodaj pliki projektów.

---
+++ SecureAccessDB/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SecureAccessDB
+{
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
